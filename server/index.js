637c6ef013a70bac0c87ae34002a198a7a6a5db4
Honorio <htaveras90@gmail.com>
2020-09-29 21:18:34 -0600

    initial app config all set up - ready to create now

---
+++ server/index.js
@@ -0,0 +1,25 @@
+/* eslint-disable no-console */
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
+const router = require('./routes.js');
+
+const app = express();
+module.exports.app = app;
+
+app.set('port', 3333);
+
+// middleware
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(morgan('dev'));
+app.use(cors());
+
+app.use(express.static('public'));
+
+// If we are being run directly, run the server.
+if (!module.parent) {
+  app.listen(app.get('port'));
+  console.log('Listening on', app.get('port'));
+}
