22979001b512ad27e6fa508128283b5a5bf272cf
Honorio <htaveras90@gmail.com>
2020-10-02 22:08:38 -0600

    updateing app with a shop page, collection preview comonent and collection item component

--- client/src/components/CollectionPreview/CollectionPreview.jsx
+++ client/src/components/CollectionPreview/CollectionPreview.jsx
@@ -1,16 +1,20 @@
+/* eslint-disable react/jsx-props-no-spreading */
 import React from 'react';
+
+import CollectionItem from '../CollectionItem/CollectionItem';
 
 import './CollectionPreview.scss';
 
 const CollectionPreview = ({ title, items }) => (
   <div className="collection-preview">
     <h1 className="title">{title.toUpperCase()}</h1>
-    <div className="preview" />
-    {items
-      .filter((item, idx) => idx < 4)
-      .map((item) => (
-        <div key={item.id}>{item.name}</div>
-      ))}
+    <div className="preview">
+      {items
+        .filter((item, idx) => idx < 4)
+        .map(({ id, ...otherItemProps }) => (
+          <CollectionItem key={id} {...otherItemProps} />
+        ))}
+    </div>
   </div>
 );
 
