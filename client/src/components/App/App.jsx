25698be37d34d61391bdb810eca7ab50124ae057
Honorio <htaveras90@gmail.com>
2020-10-06 15:58:25 -0600

    created fully functioning auth service with google on signin page

--- client/src/components/App/App.jsx
+++ client/src/components/App/App.jsx
@@ -1,4 +1,5 @@
-import React from 'react';
+/* eslint-disable react/no-unused-state */
+import React, { Component } from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 // COMPONENTS
@@ -6,20 +7,43 @@ import HomePage from '../../pages/Homepage/HomePage';
 import ShopPage from '../../pages/Shop/Shop';
 import Header from '../Header/Header';
 import SignInAndSignUp from '../../pages/SignInAndSignUp/SignInAndSignUp';
+import { auth } from '../../firebase/firebase.utils';
 
 import './App.scss';
 
-export default function App() {
-  return (
-    <Router>
-      <div>
-        <Header />
-        <Switch>
-          <Route exact path="/" component={HomePage} />
-          <Route path="/shop" component={ShopPage} />
-          <Route path="/signin" component={SignInAndSignUp} />
-        </Switch>
-      </div>
-    </Router>
-  );
+export default class App extends Component {
+  unsubscribeFromAuth = null;
+
+  constructor() {
+    super();
+    this.state = {
+      currentUser: null,
+    };
+  }
+
+  componentDidMount() {
+    this.unsubscribeFromAuth = auth.onAuthStateChanged((user) => {
+      this.setState({ currentUser: user });
+      console.log('user: ', user);
+    });
+  }
+
+  componentWillUnmount() {
+    this.unsubscribeFromAuth();
+  }
+
+  render() {
+    return (
+      <Router>
+        <div>
+          <Header />
+          <Switch>
+            <Route exact path="/" component={HomePage} />
+            <Route path="/shop" component={ShopPage} />
+            <Route path="/signin" component={SignInAndSignUp} />
+          </Switch>
+        </div>
+      </Router>
+    );
+  }
 }
