02457832385d42a8c528488286f477f8391a69b0
Honorio <htaveras90@gmail.com>
2020-10-08 16:28:03 -0600

    finished firebase sign in auth and firestore db config and functionality

--- client/src/components/App/App.jsx
+++ client/src/components/App/App.jsx
@@ -23,8 +23,21 @@ export default class App extends Component {
   }
 
   componentDidMount() {
-    this.unsubscribeFromAuth = auth.onAuthStateChanged(async (user) => {
-      createUserProfileDocument(user);
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(async (userAuth) => {
+      if (userAuth) {
+        const userRef = await createUserProfileDocument(userAuth);
+
+        userRef.onSnapshot((snapShot) => {
+          this.setState({
+            currentUser: {
+              id: snapShot.id,
+              ...snapShot.data(),
+            },
+          });
+        });
+      } else {
+        this.setState({ currentUser: userAuth });
+      }
     });
   }
 
