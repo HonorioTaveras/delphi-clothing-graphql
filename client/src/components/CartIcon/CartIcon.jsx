52041cb29ad2f34a9adb36ddb1860014aef7fd61
Honorio <htaveras90@gmail.com>
2020-10-10 19:30:37 -0600

    got rid of code-style errors in CartIcon component

--- client/src/components/CartIcon/CartIcon.jsx
+++ client/src/components/CartIcon/CartIcon.jsx
@@ -1,3 +1,6 @@
+/* eslint-disable no-shadow */
+/* eslint-disable jsx-a11y/click-events-have-key-events */
+/* eslint-disable jsx-a11y/no-static-element-interactions */
 import React from 'react';
 import { connect } from 'react-redux';
 
