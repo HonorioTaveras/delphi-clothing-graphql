0c7821fe6f70264dc49113b30a1f69d317536f23
Honorio <htaveras90@gmail.com>
2020-10-15 16:55:04 -0600

    added toggle to cart dropdown when routed to checkout page with dispatch prop shortcut in CartDropdown component

--- client/src/components/CartDropdown/CartDropdown.jsx
+++ client/src/components/CartDropdown/CartDropdown.jsx
@@ -6,21 +6,29 @@ import { withRouter } from 'react-router-dom';
 import CustomButton from '../CustomButton/CustomButton';
 import CartItem from '../CartItem/CartItem';
 import { selectCartItems } from '../../redux/cart/cartSelectors';
+import { toggleCartHidden } from '../../redux/cart/cartActions';
 
 import './CartDropdown.scss';
 
-const CartDropdown = ({ cartItems, history }) => (
+const CartDropdown = ({ cartItems, history, dispatch }) => (
   <div className="cart-dropdown">
     <div className="cart-items">
-      {
-        cartItems.length
-          ? cartItems.map((cartItem) => (
-            <CartItem key={cartItem.id} item={cartItem} />
-          ))
-          : <span className="empty-message">Your cart is empty</span>
-      }
+      {cartItems.length ? (
+        cartItems.map((cartItem) => (
+          <CartItem key={cartItem.id} item={cartItem} />
+        ))
+      ) : (
+        <span className="empty-message">Your cart is empty</span>
+      )}
     </div>
-    <CustomButton onClick={() => history.push('/checkout')}>GO TO CHECKOUT</CustomButton>
+    <CustomButton
+      onClick={() => {
+        history.push('/checkout');
+        dispatch(toggleCartHidden());
+      }}
+    >
+      GO TO CHECKOUT
+    </CustomButton>
   </div>
 );
 
