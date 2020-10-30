60127c76c9520348592883112cc37a151ca2f3d4
Honorio <htaveras90@gmail.com>
2020-10-30 16:20:03 -0600

    added bundle to eslint ignore, added google-sign-in className to custom button

--- client/src/components/CustomButton/CustomButton.jsx
+++ client/src/components/CustomButton/CustomButton.jsx
@@ -7,7 +7,9 @@ const CustomButton = ({
   children, isGoogleSignIn, inverted, ...otherProps
 }) => (
   <button
-    className={`${inverted ? 'inverted' : ''} custom-button`}
+    className={`${inverted ? 'inverted' : ''} ${
+      isGoogleSignIn ? 'google-sign-in' : ''
+    } custom-button`}
     {...otherProps}
   >
     {children}
