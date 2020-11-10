704258cfeb7c063f0eab16cfcb9eecc60cb36789
Honorio <htaveras90@gmail.com>
2020-11-10 16:26:24 -0700

    fixed bug within cart item styles

--- client/src/components/CartItem/CartItem.jsx
+++ client/src/components/CartItem/CartItem.jsx
@@ -9,7 +9,7 @@ const CartItem = ({
 }) => (
   <div className="cart-item">
     <img src={imageUrl} alt="item" />
-    <div className="image-details">
+    <div className="item-details">
       <span className="name">{name}</span>
       <span className="price">
         {' '}
