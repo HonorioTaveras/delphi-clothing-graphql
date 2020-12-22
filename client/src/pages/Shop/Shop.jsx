d351f89107679db18c3c236172228e679c7f3647
Honorio <htaveras90@gmail.com>
2020-12-21 23:32:09 -0700

    convertyed shop page into class component in order to dynamically fetch shop data from firebase datastore and pass into redux

--- client/src/pages/Shop/Shop.jsx
+++ client/src/pages/Shop/Shop.jsx
@@ -1,15 +1,24 @@
-/* eslint-disable react/jsx-props-no-spreading */
-import React from 'react';
+import React, { Component } from 'react';
 import { Route } from 'react-router-dom';
 
 import CollectionsOverview from '../../components/CollectionsOverview/CollectionsOverview';
 import CollectionPage from '../Collection/Collection';
 
-const ShopPage = ({ match }) => (
-  <div className="shop-page">
-    <Route exact path={`${match.path}`} component={CollectionsOverview} />
-    <Route path={`${match.path}/:collectionId`} component={CollectionPage} />
-  </div>
-);
+export class ShopPage extends Component {
+  unsubscribeFromSnapshot = null;
+
+  render() {
+    const { match } = this.props;
+    return (
+      <div className="shop-page">
+        <Route exact path={`${match.path}`} component={CollectionsOverview} />
+        <Route
+          path={`${match.path}/:collectionId`}
+          component={CollectionPage}
+        />
+      </div>
+    );
+  }
+}
 
 export default ShopPage;
