d707c75eba88d8ad30033496d85ad9f54abd51f8
Honorio <htaveras90@gmail.com>
2020-12-23 23:23:59 -0700

    removed linter error in shop for state in constructor

--- .eslintrc.js
+++ .eslintrc.js
@@ -19,6 +19,7 @@ module.exports = {
   },
   plugins: ['react'],
   rules: {
+    'react/state-in-constructor': 'off',
     'react/jsx-props-no-spreading': 'off',
     'react/prop-types': 'off',
     'no-use-before-define': ['off', 'nofunc'],
