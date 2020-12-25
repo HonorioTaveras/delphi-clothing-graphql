4de3c10011d2489dae8725477df0de3dd1413281
Honorio <htaveras90@gmail.com>
2020-12-25 16:00:22 -0700

    adding thunks for async code inside of shop actions in redux

--- client/src/redux/shop/shopActions.js
+++ client/src/redux/shop/shopActions.js
@@ -1,6 +1,20 @@
 import ShopActionTypes from './shopTypes';
 
-export const updateCollections = (collectionsMap) => ({
-  type: ShopActionTypes.UPDATE_COLLECTIONS,
+import {
+  firestore,
+  convertCollectionsSnapshotToMap,
+} from '../../firebase/firebase.utils';
+
+export const fetchCollectionsStart = () => ({
+  type: ShopActionTypes.FETCH_COLLECTIONS_START,
+});
+
+export const fetchCollectionsSuccess = (collectionsMap) => ({
+  type: ShopActionTypes.FETCH_COLLECTIONS_SUCCESS,
   payload: collectionsMap,
+});
+
+export const fetchCollectionsFailure = (errorMessage) => ({
+  type: ShopActionTypes.FETCH_COLLECTIONS_FAILURE,
+  payload: errorMessage,
 });
