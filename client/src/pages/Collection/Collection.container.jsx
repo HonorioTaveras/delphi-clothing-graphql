d8f1676c5aed4e0a715edefc7d4a997ccc8239a3
Honorio <htaveras90@gmail.com>
2020-12-27 18:11:53 -0700

    completed logic for collection container

--- client/src/pages/Collection/Collection.container.jsx
+++ client/src/pages/Collection/Collection.container.jsx
@@ -5,3 +5,14 @@ import { createStructuredSelector } from 'reselect';
 import { selectIsCollectionLoaded } from '../../redux/shop/shopSelector';
 import WithSpinner from '../../components/WithSpinner/WithSpinner';
 import CollectionPage from './Collection';
+
+const mapStateToProps = createStructuredSelector({
+  isLoading: (state) => !selectIsCollectionLoaded(state),
+});
+
+const CollectionPageContainer = compose(
+  connect(mapStateToProps),
+  WithSpinner,
+)(CollectionPage);
+
+export default CollectionPageContainer;
