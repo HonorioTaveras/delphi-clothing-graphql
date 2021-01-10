4084812d3bfd80975bcc82f05cd200e134d8761e
Honorio <htaveras90@gmail.com>
2021-01-09 17:42:22 -0700

    importing firebase values / methods into user sagas for google sign in

--- client/src/redux/user/userSagas.js
+++ client/src/redux/user/userSagas.js
@@ -2,10 +2,19 @@ import { takeLatest, put } from 'redux-saga/effects';
 
 import UserActionTypes from './userTypes';
 
-export function* signInWithGoogle() {
+import {
+  auth,
+  googleProvider,
+  createUserProfileDocument,
+} from '../../firebase/firebase.utils';
 
+export function* signInWithGoogle() {
+  try {
+    const userRef = yield auth.signInWithPopup(googleProvider);
+    console.log(userRef);
+  } catch (error) {}
 }
 
 export function* onGoogleSignInStart() {
-  yield takeLatest(UserActionTypes.GOOGLE_SIGN_IN_START, ),
+  yield takeLatest(UserActionTypes.GOOGLE_SIGN_IN_START);
 }
