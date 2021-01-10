acd779237679773fdf84e7df3d5ab8044728ef97
Honorio <htaveras90@gmail.com>
2021-01-09 18:20:19 -0700

    completed google sign in sagas flow

--- client/src/redux/user/userSagas.js
+++ client/src/redux/user/userSagas.js
@@ -1,4 +1,6 @@
-import { takeLatest, put, all, call } from 'redux-saga/effects';
+import {
+  takeLatest, put, all, call,
+} from 'redux-saga/effects';
 
 import UserActionTypes from './userTypes';
 
@@ -7,16 +9,23 @@ import {
   googleProvider,
   createUserProfileDocument,
 } from '../../firebase/firebase.utils';
+import { googleSignInFailure, googleSignInSuccess } from './userActions';
 
 export function* signInWithGoogle() {
   try {
-    const userRef = yield auth.signInWithPopup(googleProvider);
-    console.log(userRef);
-  } catch (error) {}
+    const { user } = yield auth.signInWithPopup(googleProvider);
+    const userRef = yield call(createUserProfileDocument, user);
+    const userSnapShot = yield userRef.get();
+    yield put(
+      googleSignInSuccess({ id: userSnapShot.id, ...userSnapShot.data() }),
+    );
+  } catch (error) {
+    yield put(googleSignInFailure(error));
+  }
 }
 
 export function* onGoogleSignInStart() {
-  yield takeLatest(UserActionTypes.GOOGLE_SIGN_IN_START);
+  yield takeLatest(UserActionTypes.GOOGLE_SIGN_IN_START, signInWithGoogle);
 }
 
 export function* userSagas() {
