1ae73176653f234a78adae9649dab5077190e7dd
Honorio <htaveras90@gmail.com>
2021-01-09 17:10:15 -0700

    added user types in redux to start refactor for sign in and sign up flow

--- client/src/components/SignIn/SignIn.jsx
+++ client/src/components/SignIn/SignIn.jsx
@@ -65,7 +65,11 @@ export default class SignIn extends Component {
           />
           <SignInButtons>
             <CustomButton type="submit"> Sign in </CustomButton>
-            <CustomButton type="button" onClick={signInWithGoogle} isGoogleSignIn>
+            <CustomButton
+              type="button"
+              onClick={signInWithGoogle}
+              isGoogleSignIn
+            >
               Sign in with Google
             </CustomButton>
           </SignInButtons>
