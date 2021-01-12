1ffe9ac43c0f0bd0b0c4831a1385a33eec2cb2bd
Honorio <htaveras90@gmail.com>
2021-01-11 18:01:00 -0700

    fixed bug in user sagas to call on correct method to watch out for

--- client/src/redux/user/userSagas.js
+++ client/src/redux/user/userSagas.js
@@ -66,6 +66,6 @@ export function* userSagas() {
   yield all([
     call(onGoogleSignInStart),
     call(onEmailSignInStart),
-    call(isUserAuthenticated),
+    call(onCheckUserSession),
   ]);
 }
