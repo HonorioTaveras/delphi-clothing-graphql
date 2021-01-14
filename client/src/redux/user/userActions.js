aa7bcc7e1e58650c5b156ee7b7091b87d784674c
Honorio <htaveras90@gmail.com>
2021-01-13 22:13:59 -0700

    added sign up types to action file

--- client/src/redux/user/userActions.js
+++ client/src/redux/user/userActions.js
@@ -35,3 +35,17 @@ export const signOutFailure = (error) => ({
   type: UserActionTypes.SIGN_OUT_FAILURE,
   payload: error,
 });
+
+export const signUpStart = () => ({
+  type: UserActionTypes.SIGN_UP_START,
+});
+
+export const signUpSuccess = (user) => ({
+  type: UserActionTypes.SIGN_UP_START,
+  payload: user,
+});
+
+export const signUpFailure = (error) => ({
+  type: UserActionTypes.SIGN_UP_START,
+  payload: error,
+});
