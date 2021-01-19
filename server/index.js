3a01c73cca24f3d46d837e026f0143ce57106f2d
Honorio <htaveras90@gmail.com>
2021-01-19 00:10:52 -0700

    changed up some of the code in preperation for stripe backend code

--- server/index.js
+++ server/index.js
@@ -1,9 +1,10 @@
-/* eslint-disable no-console */
 const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
 const router = require('./routes.js');
+
+if (process.env.NODE_ENV !== 'production') require('dotenv').config();
 
 const app = express();
 module.exports.app = app;
@@ -11,7 +12,7 @@ module.exports.app = app;
 app.set('port', process.env.PORT || 3333);
 
 // middleware
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(morgan('dev'));
 app.use(cors());
