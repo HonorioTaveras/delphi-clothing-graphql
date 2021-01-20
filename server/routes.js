d370b80790bb86badeda74803e743f624af91bef
Honorio <htaveras90@gmail.com>
2021-01-19 19:56:48 -0700

    added post route to stripe payment in order to process payments

--- server/routes.js
+++ server/routes.js
@@ -1,8 +1,25 @@
 const router = require('express').Router();
 const path = require('path');
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 
 router.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '../public/index.html'));
+});
+
+router.post('/payment', (req, res) => {
+  const body = {
+    source: req.body.token.id,
+    amount: req.body.amount,
+    currenct: 'usd',
+  };
+
+  stripe.charges.create(body, (stripeErr, stripeRes) => {
+    if (stripeErr) {
+      res.status(500).send({ error: stripeErr });
+    } else {
+      res.status(200).send({ success: stripeRes });
+    }
+  });
 });
 
 module.exports = router;
