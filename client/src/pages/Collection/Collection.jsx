6a0c01b30c94eec88d8e58d0fbaa2c4dab0cab64
Honorio <htaveras90@gmail.com>
2021-01-20 23:09:47 -0700

    added collections context to collections page component the hard way, will refactor

--- client/src/pages/Collection/Collection.jsx
+++ client/src/pages/Collection/Collection.jsx
@@ -4,28 +4,31 @@ import { connect } from 'react-redux';
 import CollectionItem from '../../components/CollectionItem/CollectionItem';
 
 import { selectCollection } from '../../redux/shop/shopSelector';
+import CollectionsContext from '../../contexts/collections/CollectionsContext';
 
 import './Collection.scss';
 
-const CollectionPage = ({ collection }) => {
-  console.log('collection: ', collection);
-  const { title, items } = collection;
-  return (
-    <div className="collection-page">
-      <h2 className="title">{title}</h2>
-      <div className="items">
-        {
-          items.map((item) => (
-            <CollectionItem key={item.id} item={item} />
-          ))
-        }
-      </div>
-    </div>
-  );
-};
+const CollectionPage = ({ match }) => (
+  <CollectionsContext.Consumer>
+    {(collections) => {
+      const collection = collections[match.params.collectionId];
+      const { title, items } = collection;
+      return (
+        <div className="collection-page">
+          <h2 className="title">{title}</h2>
+          <div className="items">
+            {items.map((item) => (
+              <CollectionItem key={item.id} item={item} />
+            ))}
+          </div>
+        </div>
+      );
+    }}
+  </CollectionsContext.Consumer>
+);
 
 const mapStateToProps = (state, ownProps) => ({
   collection: selectCollection(ownProps.match.params.collectionId)(state),
 });
 
-export default connect(mapStateToProps)(CollectionPage);
+export default CollectionPage;
