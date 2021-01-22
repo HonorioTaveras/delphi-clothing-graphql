ccbe6ed7fcad9d12e119d06f65023e771cb9bee5
Honorio <htaveras90@gmail.com>
2021-01-22 15:32:33 -0700

    added cart items from cart context and got rid of cart items from redux

--- client/src/components/CheckoutItem/CheckoutItem.jsx
+++ client/src/components/CheckoutItem/CheckoutItem.jsx
@@ -1,4 +1,3 @@
-/* eslint-disable no-shadow */
 /* eslint-disable jsx-a11y/click-events-have-key-events */
 /* eslint-disable jsx-a11y/no-static-element-interactions */
 import React, { useContext } from 'react';
