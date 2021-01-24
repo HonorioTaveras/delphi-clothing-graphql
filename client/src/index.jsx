e34913068de6e0cf0d6e6472102fdb8c6c181457
Honorio <htaveras90@gmail.com>
2021-01-23 17:27:44 -0700

    completely converted entire app to context api, deleting all redux files

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,20 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import { Provider } from 'react-redux';
-import { PersistGate } from 'redux-persist/integration/react';
-import { store, persistor } from './redux/store';
 import CartProvider from './providers/cart/CartProvider';
 
 import App from './components/App/App';
 
 ReactDOM.render(
   <CartProvider>
-    <Provider store={store}>
-      <PersistGate persistor={persistor}>
-        <App />
-      </PersistGate>
-    </Provider>
+    <App />
   </CartProvider>,
   document.getElementById('app'),
 );
--- client/src/redux/user/userActions.js
+++
@@ -1,7 +0,0 @@
-/* eslint-disable import/prefer-default-export */
-import { UserActionTypes } from './userTypes';
-
-export const setCurrentUser = (user) => ({
-  type: UserActionTypes.SET_CURRENT_USER,
-  payload: user,
-});
