a1f96bacce78c15a871f74f6cdffd110b6d258d1
Honorio <htaveras90@gmail.com>
2021-01-26 14:14:18 -0700

    onclick for collection title in collection preview component

--- client/src/components/CollectionPreview/CollectionPreview.jsx
+++ client/src/components/CollectionPreview/CollectionPreview.jsx
@@ -1,3 +1,5 @@
+/* eslint-disable jsx-a11y/click-events-have-key-events */
+/* eslint-disable jsx-a11y/no-noninteractive-element-interactions */
 /* eslint-disable react/jsx-props-no-spreading */
 import React from 'react';
 
@@ -5,9 +7,16 @@ import CollectionItem from '../CollectionItem/CollectionItem';
 
 import './CollectionPreview.scss';
 
-const CollectionPreview = ({ title, items, history, match }) => (
+const CollectionPreview = ({
+  title, items, history, match,
+}) => (
   <div className="collection-preview">
-    <h1 className="title" onClick={() => history.push(`${match.path}/${title.toLowerCase()}`)}>{title.toUpperCase()}</h1>
+    <h1
+      className="title"
+      onClick={() => history.push(`${match.path}/${title.toLowerCase()}`)}
+    >
+      {title.toUpperCase()}
+    </h1>
     <div className="preview">
       {items
         .filter((item, idx) => idx < 4)
