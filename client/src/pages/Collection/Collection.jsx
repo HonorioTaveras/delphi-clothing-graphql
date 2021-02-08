97166e193a6c9f47b95003499b00b0426befb73b
Honorio <htaveras90@gmail.com>
2021-02-08 16:49:14 -0700

    took away redux code from collection page

--- client/src/pages/Collection/Collection.jsx
+++ client/src/pages/Collection/Collection.jsx
@@ -1,9 +1,6 @@
 import React from 'react';
-import { connect } from 'react-redux';
 
 import CollectionItem from '../../components/CollectionItem/CollectionItem';
-
-import { selectCollection } from '../../redux/shop/shopSelector';
 
 import './Collection.scss';
 
@@ -14,18 +11,12 @@ const CollectionPage = ({ collection }) => {
     <div className="collection-page">
       <h2 className="title">{title}</h2>
       <div className="items">
-        {
-          items.map((item) => (
-            <CollectionItem key={item.id} item={item} />
-          ))
-        }
+        {items.map((item) => (
+          <CollectionItem key={item.id} item={item} />
+        ))}
       </div>
     </div>
   );
 };
 
-const mapStateToProps = (state, ownProps) => ({
-  collection: selectCollection(ownProps.match.params.collectionId)(state),
-});
-
-export default connect(mapStateToProps)(CollectionPage);
+export default CollectionPage;
