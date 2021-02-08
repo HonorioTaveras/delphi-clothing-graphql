b577856c54027b9c620009e4597f231841b4a41e
Honorio <htaveras90@gmail.com>
2021-02-08 16:53:58 -0700

    added collection container component, importing dependencies

---
+++ client/src/pages/Collection/CollectionContainer.jsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import { Query } from 'react-apollo';
+import { gql } from 'apollo-boost';
+
+import CollectionPage from './Collection';
+import Spinner from '../../components/Spinner/Spinner';
