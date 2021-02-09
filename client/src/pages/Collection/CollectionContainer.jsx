b4a55ea58893c60649a21896ba32caa2b1c4b95e
Honorio <htaveras90@gmail.com>
2021-02-08 17:02:06 -0700

    wrote out query for collection container

--- client/src/pages/Collection/CollectionContainer.jsx
+++ client/src/pages/Collection/CollectionContainer.jsx
@@ -4,3 +4,18 @@ import { gql } from 'apollo-boost';
 
 import CollectionPage from './Collection';
 import Spinner from '../../components/Spinner/Spinner';
+
+const GET_COLLECTIONS_BY_TITLE = gql`
+  query getCollectionsByTitle($title: String!) {
+    getCollectionsByTitle(title: $title) {
+      id
+      title
+      items {
+        id
+        name
+        price
+        imageUrl
+      }
+    }
+  }
+`;
